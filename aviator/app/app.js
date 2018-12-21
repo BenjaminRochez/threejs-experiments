3013e20e21776182941c8a069ae3aee8124d703d
Benjamin Rochez <rochez.benjamin@gmail.com>
2018-12-21 15:08:00 +0100

    Added the plane

--- aviator/app/app.js
+++ aviator/app/app.js
@@ -22,7 +22,7 @@ function init(){
     createLights();
 
     // add the objects
-    //createPlane();
+    createPlane();
     createSea();
     createSky();
     // start a loop to update the objects
@@ -286,6 +286,97 @@ function createSky(){
 	sky = new Sky();
 	sky.mesh.position.y = -600;
 	scene.add(sky.mesh);
+}
+
+
+
+/* Plane
+********************************************/
+
+var AirPlane = function(){
+    this.mesh = new THREE.Object3D();
+
+    // cockpit
+    var geomCockpit = new THREE.BoxGeometry(60,50,50,1,1,1);
+    var matCockpit = new THREE.MeshPhongMaterial({
+        color: Colors.red,
+        flatShading: true
+    });
+    var cockpit = new THREE.Mesh(geomCockpit, matCockpit);
+    cockpit.castShadow = true;
+    cockpit.receiveShadow = true;
+    this.mesh.add(cockpit);
+
+    // engine
+    var geomEngine = new THREE.BoxGeometry(20,50,50,1,1,1);
+    var matEngine = new THREE.MeshPhongMaterial({
+        color: Colors.white, 
+        flatShading: true
+    });
+    var engine = new THREE.Mesh(geomEngine, matEngine);
+    engine.position.x = 40;
+    engine.castShadow = true;
+    engine.receiveShadow = true;
+    this.mesh.add(engine);
+
+    // tail
+    var geomTailPlane = new THREE.BoxGeometry(15,20,5,1,1,1);
+    var matTailPlane = new THREE.MeshPhongMaterial({
+        color: Colors.red,
+        flatShading: true,
+    });
+    var tailPlane = new THREE.Mesh(geomTailPlane, matTailPlane);
+    tailPlane.position.set(-35,25,0);
+    tailPlane.castShadow = true;
+    tailPlane.receiveShadow= true;
+    this.mesh.add(tailPlane); 
+
+    // wing
+    var geomSideWing = new THREE.BoxGeometry(40,8,150,1,1,1);
+    var matSideWing = new THREE.MeshPhongMaterial({
+        color: Colors.red,
+        flatShading: true
+    });
+    var sideWing = new THREE.Mesh(geomSideWing, matSideWing);
+    sideWing.castShadow = true;
+    sideWing.receiveShadow = true;
+    this.mesh.add(sideWing);
+
+    // NB: we are linking the propeller and the blades for the animation later (rotate the propeller will rotate the blade auto)
+    // propeller
+    var geomPropeller = new THREE.BoxGeometry(20,10,10,1,1,1);
+    var matPropeller = new THREE.MeshPhongMaterial({
+        color: Colors.brown,
+        flatShading: true
+    });
+    this.propeller = new THREE.Mesh(geomPropeller, matPropeller);
+    this.propeller.castShadow = true;
+    this.propeller.receiveShadow = true;
+
+    // blades
+    var geomBlade = new THREE.BoxGeometry(1,100,20,1,1,1);
+    var matBlade = new THREE.MeshPhongMaterial({
+        color: Colors.brownDark,
+        flatShading: true
+    });
+    var blade = new THREE.Mesh(geomBlade, matBlade);
+    blade.position.set(8,0,0);
+    blade.castShadow = true;
+    blade.receiveShadow = true;
+    this.propeller.add(blade);
+    this.propeller.position.set(50,0,0);
+    this.mesh.add(this.propeller);
+}
+
+
+
+var airplane;
+
+function createPlane(){
+    airplane = new AirPlane();
+    airplane.mesh.scale.set(.25, .25, .25);
+    airplane.mesh.position.y = 100;
+    scene.add(airplane.mesh);
 }
 
 /* Loop
